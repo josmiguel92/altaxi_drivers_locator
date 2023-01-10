2e56c1188f5ae2c37dc56e4140563ddf1491d88b
Josue Miguel <josmiguel92@gmail.com>
2023-01-10 16:15:19 -0500

    manage error on registration

--- lib/features/auth/register/networking/register_repository.dart
+++ lib/features/auth/register/networking/register_repository.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:data_channel/data_channel.dart';
 import 'package:flutter_advanced_boilerplate/features/app/models/alert_model.dart';
 import 'package:flutter_advanced_boilerplate/features/app/models/user_model.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pocketbase/pocketbase.dart';
+import 'package:pocketbase/src/client_exception.dart';
 
 @lazySingleton
 class RegisterRepository {
@@ -25,18 +27,47 @@ class RegisterRepository {
 
     // if (isIdPwCorrect) {
     final pb = PocketBase('https://base.altaxi.app');
-    final userData = await pb.collection('drivers').create(
-      body: {
-        'email': email,
-        'password': password,
-        'passwordConfirm': passwordConfirm,
-        'username': username,
-        'phone': phone,
-      },
-    );
+    print({
+      'email': email,
+      'password': password,
+      'passwordConfirm': passwordConfirm,
+      'username': username,
+      'phone': phone,
+    });
+
+    String userId = '';
+    try {
+      final userData = await pb.collection('drivers').create(
+        body: {
+          'email': email,
+          'password': password,
+          'passwordConfirm': passwordConfirm,
+          'username': username,
+          'phone': phone,
+        },
+      );
+      userId = userData.id;
+    } on ClientException catch (error) {
+      final errorEntries = error.response['data'];
+
+      String message = '';
+      if (errorEntries.containsKey('email') != null) {
+        message = 'The email is not valid or already exist';
+      }
+      if (errorEntries.containsKey('username') != null) {
+        message = 'The username is not valid';
+      }
+
+      final alert = AlertModel.alert(
+        message: message,
+        type: AlertType.destructive,
+      );
+
+      return DC.error(alert);
+    }
 
     final user = UserModel(
-      id: userData.id,
+      id: userId,
       username: username,
       password: password,
       email: email,
